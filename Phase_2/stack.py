c15627589c71c5864bef33915e2a11f752897025
Yang_Xie <xie_yang_susu@outlook.com>
2026-04-27 21:06:48 +1000

    fix: fix the bug in the normal_method

--- Phase_2/stack.py
+++ Phase_2/stack.py
@@ -55,3 +55,97 @@ s3 = "((("
 print(stack(s1))
 print(stack(s2))
 print(stack(s3))
+
+
+'''
+栈的本质是维护**"最近未处理"**的状态，这个思想能解决一类很重要的问题：
+    给定一个数组，对于每个元素，找到它右边第一个比它大的元素。
+
+比如：
+输入：[2, 1, 4, 3, 6]
+输出：[4, 4, 6, 6, -1]
+解释：2右边第一个比它大的是4，1右边第一个比它大的是4...
+
+暴力法是 O(n^2) 但用栈可以做到 O(n)，这个技巧叫单调栈，是栈最强大的应用之一。
+'''
+
+'''
+第八题：单调栈
+
+问题描述
+给定一个整数数组 nums，对于每个元素，找到它右边第一个比它大的元素，如果不存在则返回 -1
+
+示例：
+输入：nums = [2, 1, 4, 3, 6]
+输出：[4, 4, 6, 6, -1]
+
+输入：nums = [5, 4, 3, 2, 1]
+输出：[-1, -1, -1, -1, -1]
+
+引导思考
+第一步：暴力解法是什么？
+    对每个元素，往右扫描找第一个更大的，时间复杂度是多少？
+
+第二步：暴力法浪费了什么？
+    考虑数组 [2, 1, 4, ...]，当你扫描到 4 时：
+
+    4 是 2 右边第一个更大的
+    4 也是 1 右边第一个更大的 
+
+    暴力法对 2 和 1 分别扫描了一遍，但其实 4 可以同时解决它们两个
+
+第三步：栈里存什么？
+    维护一个栈，存放还没找到答案的元素的下标。
+    遍历到一个新元素 nums[i] 时，检查栈顶元素：
+        如果 nums[i] 大于栈顶元素对应的值 → 说明什么？
+        然后继续检查新的栈顶，直到什么时候停止？
+        最后把 nums[i] 的下标压入栈
+第四步：栈的单调性
+    按照上面的逻辑，栈里的元素从栈底到栈顶，对应的值是递增还是递减的？
+    这就是"单调栈"名字的由来。
+复杂度
+    每个元素最多入栈一次、出栈一次，所以总时间复杂度是 O(n)，而不是O(n^2)
+'''
+
+def normal_method(arr):
+    result = []
+    
+    for i in range(len(arr)):
+        biggest = True
+        for j in range(i+1, len(arr)):
+            if arr[j] > arr[i]:
+                result.append(arr[j])
+                biggest = False
+                break
+        if biggest:
+            result.append(-1)
+    return result
+
+
+nums1 = [2, 1, 4, 3, 6]
+nums2 = [5, 4, 3, 2, 1]
+
+print(normal_method(nums1))
+print(normal_method(nums2))
+
+
+def monotonic_stack(arr):
+    stack = []
+    result = [None] * len(arr)
+
+    for i in range(len(arr)):
+        while stack and arr[stack[-1]] < arr[i]:
+            index = stack.pop()
+            result[index] = arr[i]
+        stack.append(i)
+    while stack:
+        index = stack.pop()
+        result[index] = -1
+
+    return result
+
+nums1 = [2, 1, 4, 3, 6]
+nums2 = [5, 4, 3, 2, 1]
+
+print(monotonic_stack(nums1))
+print(monotonic_stack(nums2))
