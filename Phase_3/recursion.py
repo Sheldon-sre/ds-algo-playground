f79e78c002849531c0567c6ade537ac38c945353
Yang_Xie <xie_yang_susu@outlook.com>
2026-05-02 00:02:21 +1000

    feat: Adding permutation function

--- Phase_3/recursion.py
+++ Phase_3/recursion.py
@@ -151,3 +151,93 @@ def backtrack(start, current):
 
 backtrack(0,[])
 print(result)
+
+
+'''
+延伸思考
+子集问题是回溯最基础的形态，试着思考两个变种：
+
+变种1：全排列
+    给定 [1,2,3]，返回所有排列：[1,2,3], [1,3,2], [2,1,3]...
+
+    和子集的区别是：每个元素必须用且只用一次，顺序不同算不同结果。start 的逻辑需要怎么改？
+
+变种2：组合总和
+    给定 [2,3,6,7] 和目标值 7，找出所有和为 7 的组合
+
+    这里需要剪枝：当当前和已经超过目标值时，没必要继续递归了
+'''
+
+'''
+第十三题：全排列
+
+问题描述
+给定一个无重复元素的整数数组 nums，返回所有可能的全排列
+
+示例：
+输入：nums = [1, 2, 3]
+输出：[[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
+
+引导思考
+第一步：和子集的区别
+子集问题中，[1,2] 和 [2,1] 是同一个子集，所以用 start 保证只往后选。
+但排列中，[1,2] 和 [2,1] 是不同的排列，每次可以从所有未使用的元素中选。
+
+第二步：怎么追踪"已使用"的元素？
+用一个 used 集合记录当前路径上已经选过的元素下标：
+
+遍历所有元素，跳过已在 used 中的
+选择一个元素后加入 used
+递归结束后从 used 中移除（回溯）
+
+第三步：终止条件
+排列问题的终止条件是：current 的长度等于 nums 的长度，说明所有元素都用上了。
+
+第四步：排列数量
+n 个元素的全排列有n!个，比子集的2^n增长更快，n=10 时已经有 3628800个排列。
+'''
+
+result = []
+nums = [1, 2, 3]
+def permutation(used):
+    if len(used) == len(nums):
+        return result.append(used[:])
+    for val in nums:
+        if val in used:
+            continue
+        used.append(val)
+        permutation(used)
+        used.pop()
+
+
+'''
+另外一个隐患：if val in used
+used 是列表，in 操作是 O(n)的。
+更好的做法是用一个 set 来追踪已使用的值，查找是 O(1)：
+used_set = set()
+# 选择时
+used_set.add(val)
+# 回溯时
+used_set.remove(val)
+但需要同时维护 current 列表来记录路径
+'''
+
+result2 = []
+used_set = set()
+def permutation_2(used, current):
+    if len(current) == len(nums):
+        return result2.append(current[:])
+    for val in nums:
+        if val in used:
+            continue
+        current.append(val)
+        used.add(val)
+        permutation_2(used, current)
+        current.pop()
+        used.remove(val)
+
+permutation([])
+print(result)
+
+permutation_2(used_set,[])
+print(result2)
