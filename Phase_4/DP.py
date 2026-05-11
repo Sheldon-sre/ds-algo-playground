6806b29841e3715a1f4fe16752f78819d551a418
Yang_Xie <xie_yang_susu@outlook.com>
2026-05-11 16:32:58 +1000

    feat: Add Knapsack problem with dp method

--- Phase_4/DP.py
+++ Phase_4/DP.py
@@ -75,3 +75,71 @@ def climb_stairs_2(n):
     return f1
 
 print(climb_stairs_2(4))
+
+
+'''
+第十五题：0/1背包问题
+
+问题描述
+有一个容量为 W 的背包，和 n 个物品，每个物品有重量 w[i] 和价值 v[i]。
+每个物品只能选一次（选或不选），求背包能装下的最大价值。
+
+示例：
+输入：W = 5, w = [1, 2, 3], v = [1, 4, 5]
+输出：9
+解释：选第2件(重2,价值4)和第3件(重3,价值5)，总重5，总价值9
+
+引导思考
+第一步：状态定义
+定义 dp[i][j] = 前 i 个物品，背包容量为 j 时的最大价值。
+
+最终答案是什么？
+对于第 i 个物品，只有两种选择：
+
+第二步：状态转移
+不选：dp[i][j] = dp[i-1][j]
+选：前提是 j >= w[i]，此时 dp[i][j] = dp[i-1][j-w[i]] + v[i]
+
+两种选择取最大值：
+dp[i][j]=max⁡(dp[i−1][j], dp[i−1][j−w[i]]+v[i])
+
+第三步：初始条件
+dp[0][j] = 0：没有物品时价值为0
+dp[i][0] = 0：背包容量为0时价值为0
+
+第四步：填表顺序
+dp[i][j] 依赖 dp[i-1][...]，所以外层循环遍历物品，内层循环遍历容量，从小到大填表。
+画一下这个表格，W=5, w=[1,2,3], v=[1,4,5]
+'''
+
+# def Knapsack_problem(w,v,W):
+#     MAX = 0
+#     dp = [[0] * (W+1) for _ in range(len(w)+1)]
+#     for i in range(len(w)+1):
+#         if i == 0:
+#             continue
+#         for j in range(W+1):
+#             if j == 0:
+#                 continue
+#             if j >= w[i-1]:
+#                 dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i-1]]+v[i-1])
+#             else:
+#                 dp[i][j] = dp[i-1][j]
+#             if dp[i][j] > MAX:
+#                 MAX = dp[i][j]
+#     return MAX
+# 一个小简化：其实不需要追踪 MAX，因为最优答案一定在最后一行最后一列：
+def Knapsack_problem(w,v,W):
+    dp = [[0] * (W+1) for _ in range(len(w)+1)]
+    for i in range(len(w)+1):
+        if i == 0:
+            continue
+        for j in range(W+1):
+            if j == 0:
+                continue
+            if j >= w[i-1]:
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i-1]]+v[i-1])
+            else:
+                dp[i][j] = dp[i-1][j]
+    return dp[len(w)][W]
+print(Knapsack_problem([1,2,3], [1,4,5], 5))
